d04eacd5f57496b80a8899cfcb74ffff9f376f19
Evan Engels <evan.engels@gmail.com>
2017-02-14 13:31:29 -0600

    updated the deep equal function

--- app/arrays/deep-equal.js
+++ app/arrays/deep-equal.js
@@ -1,7 +1,17 @@
 export default function (a, b) {
-  if (a[0] !== b[0] || a[1] !== b[1] || a.length !== b.length) {
+  // if (a[0] !== b[0] || a[1] !== b[1] || a.length !== b.length) {
+  //   return false;
+  // } else {
+  //   return true;
+  // }
+  if (a.length !== b.length) {
     return false;
-  } else {
-    return true;
   }
+  for (let i = 0; i < a.length; i += 1) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
