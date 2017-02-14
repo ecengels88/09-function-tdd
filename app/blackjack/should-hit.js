3164a0ea6798d2f209c65c939c3b6ed25ac41fb0
Evan Engels <evan.engels@gmail.com>
2017-02-13 18:18:55 -0600

    array first value

--- app/blackjack/should-hit.js
+++ app/blackjack/should-hit.js
@@ -1,3 +1,10 @@
 import cardSum from './card-sum';
+import cardSplit from './should-split';
 
-export default function () {}
+export default function (card1, card2) {
+  if (cardSplit(card1, card2)) {
+    return false;
+  }
+
+  return cardSum(card1, card2) <= 17;
+}
